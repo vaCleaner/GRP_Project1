6b0eaedbf2df2e86f054a1334e45f43b56e88fdd
YolowBo <155929975+YolowBo@users.noreply.github.com>
2024-02-05 13:48:23 +0800

    Bo Wen part done
    -Camera
    -Abnormal Report
    -Different Camera Scene
    -Clock/Timer
    -Spawn Abnormal

---
+++ Grp_Project/Assets/Scripts/BoWen_Script/LevelData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class LevelData : ScriptableObject
+{
+    public string Name;
+    public int[] EverySecTier;
+
+}
---
+++ Grp_Project/Assets/Scripts/BoWen_Script/ListOfAbnormal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class AllAbnormlData
+{
+    public AbnormalAction AbnormalData;
+    public float ChanceRateMinimum;
+    public float ChanceRateMaximum;
+} 
+
+[CreateAssetMenu]
+public class ListOfAbnormal : ScriptableObject
+{
+  public AllAbnormlData[] ListOfAbnormalData;
+}
